bb6f72a9b6105a71b1bb60f1f68d86b87d671f78
Alejo Martín Arias Filippo <alejomaf@outlook.com>
2021-04-12 02:14:39 +0200

    Starting to work with Angular

---
+++ Website/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent,LoginComponent]
+})
+export class AppModule { }
