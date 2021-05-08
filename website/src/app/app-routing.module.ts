ee2bbcbd5c6d1023a1e55febfc6dfc86857bba7a
Alejo Martín Arias Filippo <alejomaf@outlook.com>
2021-05-08 19:36:41 +0200

    Solving some problems part 2

---
+++ website/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LibraryComponent } from './components/library/library.component';
+
+const routes: Routes = [
+  { path: 'libraries', component: LibraryComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],  
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ website/src/app/app.module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RegisterComponent } from './components/register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { LibraryComponent } from './components/library/library.component';
+import { BookComponent } from './components/book/book.component';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { BookCalendarComponent } from './components/book-calendar/book-calendar.component';
+import { LibraryCreateComponent } from './components/library-create/library-create.component';
+import { BookCreateComponent } from './components/book-create/book-create.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    LibraryComponent,
+    BookComponent,
+    BookCalendarComponent,
+    LibraryCreateComponent,
+    BookCreateComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    FontAwesomeModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  exports: [BookCalendarComponent]
+})
+export class AppModule { }
